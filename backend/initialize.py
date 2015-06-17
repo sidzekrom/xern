d6152cb1a75fe740a9e48d06dad8817bf761fd0c
Sidhanth Mohanty <sidhanthmohanty@Sidhanths-MacBook-Pro-2.local>
2015-06-17 18:18:10 +0530

    tagvectors for entities, rough structure for database

--- backend/initialize.py
+++ backend/initialize.py
@@ -1,10 +1,27 @@
-from *.py import *
+from tagVector import *
 import sys
 
 globalTagData = open('globalTag.txt', 'r+')
 
-tagVectorData = globalTagData.readline()
-tagStrings = tagVectorData.split(" ")
-sizeTagStrings = len(tagStrings)
+tagSize = globalTagData.readline()
+sizeTagStrings = int(tagSize)
 
-globeoftag = globalTag(tagStrings, sizeTagStrings)
+tagStrings = []
+tagStringSet = {}
+
+for i in range(sizeTagStrings):
+    stringOnLine = (globalTagData.readline()).split(" ")
+    #the line read contains a tag and the number of times it
+    #has been typed in
+    stringParse = stringJoin(stringOnLine[0:len(stringOnLine)-1])
+    tagStringSet[stringParse] = int(stringOnLine[len(stringOnLine)-1])
+    tagStrings.append(stringOnLine[0])
+
+globeoftag = globalTag(tagStrings, sizeTagStrings, tagStringSet)
+globalTagData.close()
+
+globeoftag.update("Mehul_Mehul")
+globeoftag.update("Mehul")
+globeoftag.update("sjfng")
+globeoftag.update("Mehul Mehul-Mehul")
+globeoftag.update("Preetham Ravi Preethamdeesan")
--- backend/tagVector.py
+++ backend/tagVector.py
@@ -1,32 +1,79 @@
 import numpy as np
 
+'''
+globalTag : class with string list, size of string list
+            and dictionary with a mapping from tags to ints
+'''
 class globalTag:
-    def __init__(self, tagStrings, size):
+    '''
+    __init__ : string list * int * dict -> globalTag class
+    Summoned using globalTag(tagStrings,size,tagSet)
+    Requires : size = len(tagStrings)
+    Ensures : returns a globalTag object with the inputted
+              initialized parameters
+    '''
+    def __init__(self, tagStrings, size, tagSet):
         self.tagStrings = tagStrings
+        self.tagSet = tagSet
         self.size = size
+    '''
+    update : string -> void
+    Requires : Nothing
+    Ensures : updates the globalTag so that newTag
+              is stored as a user accessible tag
+    '''
     def update(self, newTag):
-        self.tagStrings.append(newTag)
-        self.size += 1
+        newTag = self.stringJoin(newTag.split(" "))
+        if (not (newTag in self.tagSet)):
+            self.tagStrings.append(newTag)
+            self.tagSet[newTag] = 1
+            self.size += 1
+        elif(newTag in self.tagSet):
+            self.tagSet[newTag] += 1
+        globalTagData = open('globalTag.txt', 'w')
+        globalTagData.write(str(self.size))
+        globalTagData.write('\n')
+        for i in range(len(self.tagStrings)):
+            toWrite = self.tagStrings[i] + " "\
+                + str(self.tagSet[self.tagStrings[i]]) + "\n"
+            globalTagData.write(toWrite)
+        globalTagData.close()
+    '''
+    stringJoin : string list -> string
+    Requires : input is nonempty
+    Ensures : concatenates elements of the list by underscore ("_")
+    '''
+    def stringJoin(self, listString):
+        if (len(listString) == 1):
+            return listString[0]
+        elif (len(listString) > 1):
+            return (listString[0] + "_" + self.stringJoin(listString[1:]))
 
 
+'''
+tagvector is a class that contains information related to the weight
+assigned to each tag for a user it contains a dictionary that maps tags
+to the weights for each user for the tag
+'''
 class tagVector:
     '''
-    - each dimension is a tag
-    - and there is a numerical value along each dimension
-    - if a numerical value has not been initialized in a dimension,
-      then by default set it to 1/(sqrt(dimension number))
-    
-    Stuff to figure out:
-    - When a new tag is created how do we add it to
-      the tag vector of every user?
-    - Figure out the math behind normalization etc. for events
-    - Representation of tagvectors (storing numpy vector indices
-      in a dictionary etc). 
+    '''
+    '''
+    __init__, summoned using tagVector() initializes an empty
+    dictionary through tagVector().
     '''
     def __init__(self):
         self.diction = {}
+    '''
+    retrieve : string -> float
+    requires : the input string is a valid tag in the global database
+    ensures: retrieve returns the value associated with the specific
+             tag by returning the value attached to the tag
+             in the dictionary
+    '''
     def retrieve(tag):
         if (tag in self.diction):
             return self.diction[tag]
         else:
             self.diction[tag] = 1.0 / (float(globalTag.size) ** 0.5)
+            return self.diction[tag]
