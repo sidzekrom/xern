1688a8df6bb46b0c65a7575014772cb411ad6e2a
Praful Shankar <prflshankar@gmail.com>
2015-07-01 12:00:17 +0530

    first test of code

---
+++ backend/test_user.py
@@ -0,0 +1,28 @@
+#NOTE: lat,long,alt are deined in location class
+
+#global locations = *list of locations aggregated from huge groups: ['wheeler55', 'soda121', 'evans101', 'wean6', 'doe', 'gates', 'campanile', 'uc']
+
+#global users = ['prflshankar', 'sidzekrom', 'gpree', 'smehul', 'santoshval', 'fmlesh', 'dhanalol', 'adolin']
+
+#every local user: ['user', 'first_name', 'last_name', 'age', 'friends_list', 'curr_location', 'courses', 'interests', 'tags', 'personal_loations']
+
+#local users:{}
+
+user1 = {'user': 'prflshankar', 'first_name': 'praful', 'last_name': 'shankar', 'age': 18, 'friends_list': ['sidzekrom', 'smehul', 'dhanalol', 'santoshval'], 'curr_location': {'lat': 'x', 'long': 'y', 'alt':'z'}, 'courses': ['cs70', 'physics7c', 'cs61b', 'math54'], 'interests': ['math', 'cs', 'soccer', 'showdown', 'travel', 'cooking', 'troll_page', 'mun'], 'tags': 'computed values of independent classes classification and sigmoid and their respective key', 'personal_locations': ['list of self-tagged locations']} 
+#(eg:[home, samhome, toss])
+
+user2 = {'user': 'sidzekrom', 'first_name': 'sidhanth', 'last_name': 'mohanty', 'age': 18, 'friends_list': ['gpree', 'smehul', 'dhanalol', 'prflshankar'], 'curr_location': {'lat': 'x', 'long':'y', 'alt': 'z'}, 'courses': ['cs15210', 'math21235', 'cs15859', 'cs15451'] ,'interests': ['math', 'cs', 'showdown', 'pokemon', 'stormlight', 'mistborn', 'fantasy', 'mun'], 'tags': 'computed values of independent classes classification and sigmoid and their respective key', 'personal_locations': ['list of self-tagged locations']} 
+#(eg:[home, jacobhome, littleasia])
+
+user3 = {'user': 'gpree', 'first_name': 'preetham','last_name': 'gunalan','age': 18, 'friends_list': ['sidzekrom', 'smehul', 'santoshval'], 'curr_location': {'lat': 'x', 'long': 'y', 'alt':'z' }, 'courses': ['cs101', 'design722', 'rhetoricr1b', 'manga24'], 'interests': ['math', 'manga', 'design', 'television', 'youtube', 'troll_page', 'soccer', 'showdown'],'tags': 'computed values of independent classes classification and sigmoid and their respective key', 'personal_locations': ['list of self-tagged locations']}
+
+user4 = {'user': 'smehul', 'first_name': 'mehul', 'last_name': 'shankar', 'age': 18, 'friends_list': ['sidzekrom', 'prflshankar', 'gpree', 'fmlesh', 'santoshval'], 'curr_location': {'lat':'x', 'long': 'y', 'alt': 'z'}, 'courses': ['BiPC', 'med', 'youtube', 'genetics'], 'interests': ['youtube', 'manga', 'med', 'television', 'truant', 'troll_page', 'basketball', 'nba', 'showdown'], 'tags': 'computed values of independent classes. classification and sigmoid and their respective key', 'personal_locations': ['list of self-tagged locations']}
+
+user5 = {'user': 'santoshval','first_name': 'santosh','last_name': 'vallabhaneni','age': 19, 'friends_list': ['gpree', 'prflshankar', 'smehul', 'fmlesh'], 'curr_location': {'lat': 'x', 'long': 'y', 'alt': 'z'}, 'courses': ['math101', 'douche101', 'lies201'], 'interests': ['basketball', 'penn', 'mun', 'ivy', 'piano'], 'tags': 'computed values of independent classes. classification and sigmoid and their respective key', 'personal_locations': ['list of self-tagged locations']}
+
+
+user6 = {'user': 'fmlesh','first_name': 'sailesh','last_name': 'yarlagadda','age': 19,'friends_list': ['smehul', 'santoshval'],'curr_location': {'lat':'x', 'long': 'y', 'alt': 'z'},'courses': ['calc3', 'econ101', 'hume20', 'nba2k'],'interests': ['basketball', 'uwmadison', 'television', 'chem101'],'tags': 'computed values of independent classes. classification and sigmoid and their respective key','personal_locations': ['list of self-tagged locations']}
+
+user7 = {'user': 'dhanalol','first_name': 'dhananjay','last_name': 'cook','age': 18,'friends_list': ['sidzekrom', 'prflshankar'],'curr_location': {'lat':'x', 'long': 'y', 'alt': 'z'}, 'courses': ['main', 'dessert', 'starters','goodpoints'],'interests': ['food', 'mobile'],'tags': 'computed values of independent classes. classification and sigmoid and their respective key','personal_locations': ['list of self-tagged locations']}
+
+user8 = {'user': 'adolin','first_name': 'pika','last_name': 'chu','age': 100,'friends_list': [],'curr_location': {'lat': 'x', 'long': 'y', 'alt': 'z'},'courses': ['EnM', 'poke'],'interests': ['manga', 'showdown'],'tags': 'computed values of independent classes. classification and sigmoid and their respective key','personal_locations': ['list of self-tagged locations']}
