503d093c5486fe60ae7888b1ea5971d76bffaa11
Sidhanth Mohanty <sidhanthmohanty@Sidhanths-MacBook-Pro-2.local>
2015-07-06 18:22:15 +0530

    changed tags.js

--- frontend/tags.js
+++ frontend/tags.js
@@ -7,19 +7,18 @@ xern = 'mongodb://localhost:27017/xernbase'
 var arrayToReturn = 2;
 /* toFind is a dictionary, toSearch is a collection
     (referred to by its name in a string) */
-var findElem = function(toSearch, toFind, callback) {
-                    var arrayToReturn = 6;
+
+function setOutput(err, docs) {
+    arrayToReturn = docs;
+}
+
+var findElem = function(toSearch, toFind) {
                     MongoClient.connect(xern, function (err, db) {
                         coll = db.collection(toSearch);
                         tempCurs = coll.find(toFind);
-                        tempCurs.toArray(function(err, docs) {
-                            arrayToReturns = docs;
-                            callback(docs);
-                        });
-                        console.log(arrayToReturn);
+                        tempCurs.toArray(setOutput);
                     });
                     return arrayToReturn;
                };
-//findElem('userCollection', {});
-console.log(findElem('userCollection', {}, function(docs) {
-    return docs;}));
+
+console.log(findElem('userCollection', {}));
