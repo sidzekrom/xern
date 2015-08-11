63665bf4e4a5aa4e917596281983689dc714c45d
Sidhanth Mohanty <sidhanth@cmu.edu>
2015-08-11 12:33:14 +0530

    Login/logout works cleanly. Must modularize! Approximate structure would be to trim down each function in login.py by making each subprocess a function in a separate file under a class for basic flask commands

--- frontend/login.py
+++ frontend/login.py
@@ -2,35 +2,40 @@ from flask import *
 import sys
 sys.path.insert(0, '../backend')
 from locations import *
+import os
+seckey = 'tM\x14\xdcS\xa3#0\xf0\xf5xeD\x7fW\xa3\xee:\xd3\xf6\xa6\xb9\x91\xa6'
 
 app = Flask(__name__)
 
-@app.route('/<user>', methods = ['GET', 'POST'])
-def home(user):
-    if (user == ""):
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if (request.method == "POST"):
+        if (request.form['submit'] == 'sign out'):
+            session.pop('user', None)
+    if ('user' not in session):
         return redirect(url_for('login'))
-    return render_template('home.html', username = user)
+    return render_template('home.html', username = session['user'])
 
 @app.route('/login', methods = ['GET', 'POST'])
-def login(error = "Gundumeshwar"):
+def login(error = ""):
     if (request.method == "POST"):
         if (request.form['submit'] == 'Login'):
             username = request.form['username']
             givenpass = request.form['password']
             if (not actionInst.userExists(username)):
-                return redirect(url_for('home',\
-                    username = "Not a valid user"))
+                return redirect(url_for('home'))
+                session['user'] = "Not a valid user"
             actualpass = mongohelper.retrieve(['password'], user_collection,\
                     to_search = {'user' : username})
             if (actualpass != givenpass):
-                return redirect(url_for('home',\
-                                    user = "imposter"))
+                session['user'] = 'imposter'
+                return redirect(url_for('home'))
             else:
-                print url_for('home', user = username)
-                return redirect(url_for('home', user = username))
-        else:
+                session['user'] = username
+                return redirect(url_for('home'))
+        else: #else it is 'Create new account'
             return redirect(url_for('signup'))
-    return render_template('login.html', error = error)
+    return render_template('login.html')
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
@@ -44,5 +49,6 @@ def signup():
            up on Xern! You may log in now :)"))
     return render_template('createaccount.html')
 
+app.secret_key = seckey
 app.debug = True
 app.run()
